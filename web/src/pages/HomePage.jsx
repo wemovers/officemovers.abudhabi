81ef29e278f645a67d500ab0dcd12897d3d8c3b2
eemankhalid <eemankhalid04@gmail.com>
2025-10-07 20:51:35 +0500

    committing-eeman-sections

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -4,13 +4,17 @@ import ReachOutToUs from '../components/ReachOutToUs'
 import OurOfficeMovingServices from '../components/OurOfficeMovingServices'
 import SafePackingMoving from '../components/SafePackingMoving'
 import FAQs from '../components/Faqs'
+import Hero from '../components/Hero'
+import Blog from '../components/Blog'
 const HomePage = () => {
   return (
     <div>
+      <Hero/>
       <TrustBrandConnection/>
       <SafePackingMoving/>
       <OurOfficeMovingServices/>
       <FAQs/>
+      <Blog/>
       <ReachOutToUs/>
     </div>
   )
